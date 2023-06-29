6e0dd1ae2193b4ae72f3f4ccbe17ec942b799000
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-06-29 12:21:36 +0900

    chiamata axios ok

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,39 @@
 <script>
 // Importazione dello store (state management)
   import { store } from "./store"
+  import axios from "axios";
   export default{
     name        : "App",
-    components  : {}
+    components  : {},
+    data()
+    {
+      return {
+                projects  : [],
+                api_url   : 'http://127.0.0.1:8000'
+            }
+    },
+    created()
+    {},
+    mounted()
+    {
+      this.axios_call();
+    },
+    methods:
+    {
+      axios_call()
+      {
+        axios.get(`${this.api_url}/api/projects`).then( response =>
+        {
+          this.projects = response.data.projects;
+          console.log(this.projects);
+        });
+      }
+    }
   }
 </script>
 
 <template>
-  <h1>hello world</h1>
+  <h1 class="text-black bg-info">hello world</h1>
 </template>
 
 <style scoped lang="scss">
