08287a98dca275c3f4802d386860d92160b31ea8
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-07-03 20:44:01 +0900

    Primi metodi spostati da app a store

---
+++ src/AxiosStore.js
@@ -0,0 +1,39 @@
+import { reactive } from "vue";
+import axios from 'axios';
+
+export const store = reactive(
+    {
+        session_start           :   true,
+        projects                :   [],
+        categories              :   [],
+        technologies            :   [], 
+        current_page            :   '',
+        api_url_root            :   'http://127.0.0.1:8000',
+        side_panel_visible      :   true,
+
+        get_categories()
+        {
+            axios.get(`${this.api_url_root}/api/categories`)
+                .then( response =>
+                  {
+                    this.categories = response.data.categories;
+                  })
+                .catch( error =>
+                  {
+                    console.log("errore con categories");
+                  });
+        },
+
+        get_technologies()
+        {
+          axios.get(`${this.api_url_root}/api/technologies`)
+            .then( response =>
+              {
+                this.technologies = response.data.technologies;
+              })
+            .catch( error =>
+              {
+                console.log("errore con technologies");
+              });
+        }
+    })
